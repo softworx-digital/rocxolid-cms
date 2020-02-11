f95520c05fa15ff1effbb8709c66536e37d7c2ed
Peter Bolemant <peter.bolemant@softworx.digital>
2020-02-11 16:35:56 +0100

    general update

--- resources/views/model-viewers/page-element-viewer/include/images.blade.php
+++ resources/views/model-viewers/page-element-viewer/include/images.blade.php
@@ -1,4 +1,4 @@
-<ul id="{{ $component->getDomId('images', $component->getModel()->id) }}" class="list-unstyled images col-xs-12 sortable ajax-overlay" data-update-url="{{ $component->getModel()->getControllerRoute('reorder', [ 'relation' => 'images' ]) }}">
+<ul id="{{ $component->getDomId('images', $component->getModel()->id) }}" class="list-unstyled images text-center col-xs-12 sortable ajax-overlay" data-update-url="{{ $component->getModel()->getControllerRoute('reorder', [ 'relation' => 'images' ]) }}">
 @foreach ($component->getModel()->images as $image)
     <li id="{{ $component->getDomId('gallery-items', get_class($image), $image->id) }}" class="d-inline-block" data-item-id="{{ $image->id }}" data-item-type="{{ get_class($image) }}">
         <div class="img img-small @if ($image->is_model_primary) highlight @endif" @if ($image->is_model_primary) title="{{ __('rocXolid:admin::general.text.image-primary') }}" @endif>
