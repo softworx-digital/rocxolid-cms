eac4e43b02b8ac08979cbd3d616f37b6406ee56b
Peter Bolemant <peter@softworx.digital>
2020-11-19 08:18:18 +0000

    general update

--- src/Http/Controllers/AbstractCrudController.php
+++ src/Http/Controllers/AbstractCrudController.php
@@ -7,6 +7,13 @@ use Softworx\RocXolid\Http\Controllers\AbstractCrudController as RocXolidAbstrac
 // rocXolid admin components
 use Softworx\RocXolid\Admin\Components\Dashboard\Crud as CrudDashboard;
 
+/**
+ * rocXolid CMS CRUD controller.
+ *
+ * @author softworx <hello@softworx.digital>
+ * @package Softworx\RocXolid\CMS
+ * @version 1.0.0
+ */
 abstract class AbstractCrudController extends RocXolidAbstractCrudController
 {
     protected static $dashboard_type = CrudDashboard::class;
