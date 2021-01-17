d0552583bbbdf3d09d4e7f79d1c3e476ebf3f5d3
Peter Bolemant <peter@softworx.digital>
2021-01-17 17:00:18 +0000

    templates cleanup

--- resources/views/model-viewers/document-viewer/include/footer-panel.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="x_footer" style="margin-top: 0; border: 0;">
-@can ('backAny', $component->getModel())
-    <a class="btn btn-default" href="{{ $component->getController()->getRoute('index') }}"><i class="fa fa-chevron-left margin-right-10"></i>{{ $component->translate('button.back') }}</a>
-@endcan
-@can ('update', $component->getModel())
-    <a href="{{ $component->getModel()->getControllerRoute('edit') }}" class="btn btn-primary pull-right"><i class="fa fa-pencil margin-right-10"></i>{{ $component->translate('button.edit') }}</a>
-@endcan
-</div>
